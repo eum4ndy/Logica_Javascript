cc91581d69f4aba044ba1773cc40d4ceb10a6eaa
eum4ndy <163136667+eum4ndy@users.noreply.github.com>
2024-08-28 07:59:07 -0300

    declarando variaveis

---
+++ index.js
@@ -0,0 +1,26 @@
+var nome = "John";
+let idade = 25;
+const pi = 3.14;
+
+let a = 5;
+let b = 10;
+let soma = a + b;
+console.log (soma);
+
+let a = 5;
+let b = 10;
+function multiplica (x, y) {
+    return x * y;
+}
+let produlto = multiplica (a, b); 
+console.log (produlto);
+
+let frutas = ['Apple', 'Banana', 'Cherry'];
+console.log (frutas[1]);
+
+    let pessoa = {
+     primeiroNome: 'John',
+     sobrenome: 'Delon',
+     age: 25  
+    };
+    console.log (pessoa.primeiroNome);   
